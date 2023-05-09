4ecc8c5b52ae102f5d7b6411b90aa947692222a2
Albert <wensinlor@gmail.com>
2023-05-10 02:04:22 +0800

    updated
    Signed-off-by: Albert <wensinlor@gmail.com>

--- pose_estimation/robot/motor.py
+++ pose_estimation/robot/motor.py
@@ -1,108 +1,74 @@
-import time
 import RPi.GPIO as GPIO
-import curses
+import time
+import sys
+import tty
+import termios
 
+# GPIO pins for motor control
+# Modify these pins according to your setup
+enA_pin = 17
+in1_pin = 18
+in2_pin = 27
+enB_pin = 22
+in3_pin = 23
+in4_pin = 24
 
-class Motor():
-    def __init__(self, ENA, IN1, IN2, ENB, IN3, IN4):
-        """
-        Initialize the GPIO pins
-        """
-        self.ENA = ENA
-        self.IN1 = IN1
-        self.IN2 = IN2
-        self.ENB = ENB
-        self.IN3 = IN3
-        self.IN4 = IN4
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.ENA, GPIO.OUT)
-        GPIO.setup(self.IN1, GPIO.OUT)
-        GPIO.setup(self.IN2, GPIO.OUT)
-        GPIO.setup(self.IN3, GPIO.OUT)
-        GPIO.setup(self.IN4, GPIO.OUT)
-        GPIO.setup(self.ENB, GPIO.OUT)
+# Setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(enA_pin, GPIO.OUT)
+GPIO.setup(in1_pin, GPIO.OUT)
+GPIO.setup(in2_pin, GPIO.OUT)
+GPIO.setup(enB_pin, GPIO.OUT)
+GPIO.setup(in3_pin, GPIO.OUT)
+GPIO.setup(in4_pin, GPIO.OUT)
 
-        # Speed of the motors
-        speed = 0
+# Function to control the motors
+def control_motors(left_speed, right_speed):
+    GPIO.output(in1_pin, GPIO.HIGH if left_speed > 0 else GPIO.LOW)
+    GPIO.output(in2_pin, GPIO.HIGH if left_speed < 0 else GPIO.LOW)
+    GPIO.output(in3_pin, GPIO.HIGH if right_speed > 0 else GPIO.LOW)
+    GPIO.output(in4_pin, GPIO.HIGH if right_speed < 0 else GPIO.LOW)
+    pwm_left.ChangeDutyCycle(abs(left_speed))
+    pwm_right.ChangeDutyCycle(abs(right_speed))
 
-        # Set up PWM for motor control
-        self.pwm_a = GPIO.PWM(self.ENA, 100)
-        self.pwmA.start(speed)
-        self.pwm_b = GPIO.PWM(self.ENB, 100)
-        self.pwmB.start(speed)
+# Function to get keyboard input
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
-    # Motor control functions
-    def forward():
-        GPIO.output(IN1, True)
-        GPIO.output(IN2, False)
-        GPIO.output(IN3, True)
-        GPIO.output(IN4, False)
+# Main program
+if __name__ == "__main__":
+    try:
+        # Set up PWM for motor speed control
+        pwm_left = GPIO.PWM(enA_pin, 100)
+        pwm_right = GPIO.PWM(enB_pin, 100)
+        pwm_left.start(0)
+        pwm_right.start(0)
 
-    def backward():
-        GPIO.output(IN1, False)
-        GPIO.output(IN2, True)
-        GPIO.output(IN3, False)
-        GPIO.output(IN4, True)
+        while True:
+            char = getch()
+            if char == "w":
+                control_motors(50, 50)  # Forward with equal speed
+            elif char == "s":
+                control_motors(-50, -50)  # Backward with equal speed
+            elif char == "a":
+                control_motors(-50, 50)  # Turn left
+            elif char == "d":
+                control_motors(50, -50)  # Turn right
+            elif char == "x":
+                control_motors(0, 0)  # Stop
+            elif char == "q":
+                break
 
-    def left():
-        GPIO.output(IN1, True)
-        GPIO.output(IN2, False)
-        GPIO.output(IN3, False)
-        GPIO.output(IN4, True)
+    except Exception as e:
+        print(e)
 
-    def right():
-        GPIO.output(IN1, False)
-        GPIO.output(IN2, True)
-        GPIO.output(IN3, True)
-        GPIO.output(IN4, False)
-
-    def stop():
-        GPIO.output(IN1, False)
-        GPIO.output(IN2, False)
-        GPIO.output(IN3, False)
-        GPIO.output(IN4, False)
-
-def movement(stdscr):
-    # Clear the screen and set up the curses environment
-    stdscr.clear()
-    curses.curs_set(0)
-    stdscr.nodelay(True)
-
-    # Loop until the user presses the Escape key
-    while True:
-        # Wait for user input
-        key = stdscr.getch()
-        #, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN
-        # Check if the user pressed an arrow key
-        if key in [curses.KEY_LEFT]:
-            motor.left()
-        elif key in [curses.KEY_RIGHT]:
-            motor.right()
-        elif key in [curses.KEY_UP]:
-            motor.forward()
-        elif key in [curses.KEY_DOWN]:
-            motor.backward()
-        # Check if the user pressed the Escape key
-        elif key == 27:
-            break
-        else: 
-            motor.stop()
-
-        # Refresh the screen
-        stdscr.refresh()
-
-if __name__ == '__main__':
-    #set up IO pins
-    ENA = 29
-    IN1 = 31
-    IN2 = 33
-    IN3 = 36
-    IN4 = 38
-    ENB = 40
-    motor = Motor(ENA, IN1, IN2, ENB, IN3, IN4)
-
-    # Set up the curses environment and run the main function
-    curses.wrapper(movement)
-
-    # Cleanup GPIO pins
-    GPIO.cleanup()
+    finally:
+        # Clean up GPIO
+        GPIO.cleanup()
